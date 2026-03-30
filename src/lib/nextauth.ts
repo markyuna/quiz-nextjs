0ef71f21dd805c0e468559dd4025b922180d1aa6
MARCOS <marcossuarezr88@gmail.com>
2026-03-31 01:37:27 +0200

    testing auth with google

--- src/lib/nextauth.ts
+++ src/lib/nextauth.ts
@@ -1,4 +1,3 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import {
   getServerSession,
   type DefaultSession,
@@ -6,22 +5,15 @@ import {
 } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-import { prisma } from "@/lib/db";
-
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
-      id: string;
+      id?: string;
     } & DefaultSession["user"];
-  }
-
-  interface User {
-    id: string;
   }
 }
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/login",
@@ -32,14 +24,6 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  callbacks: {
-    async session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
 };
 
 export function getAuthSession() {
